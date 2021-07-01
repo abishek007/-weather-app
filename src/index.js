1ab32dcd33e6286dced9444abde23cb17cc82f95
abishek.d <abisheksrv@gmail.com>
2021-07-01 22:34:56 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -13,4 +13,16 @@ ReactDOM.render(
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./service-worker.js')
+  let deferredPrompt
+  window.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e
+
+    console.log('beforeinstallprompt fired', deferredPrompt)
+
+    deferredPrompt.userChoice
+      .then((outcome) => {
+        console.log(`User choice to install prompt: ${outcome}`)
+      })
+
+  })
 }
